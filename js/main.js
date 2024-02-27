3a2c0c20c72a710eb3ea39f574f56dc914154751
Simon <simon.bengtsson@elev.ga.ntig.se>
2024-02-27 10:37:36 +0100

    update nav

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const nav = document.querySelector('.nav2'); // Updated class name
-    const main = document.querySelector('.main2'); // Updated class name
 
     // Function to toggle the visibility of the navigation menu
     function toggleNav() {
         if (nav.classList.contains('open')) {
             nav.style.left = '-200px';
-            main.style.marginLeft = '0';
             nav.classList.remove('open');
         } else {
             nav.style.left = '0px';
-            main.style.marginLeft = '200px';
             nav.classList.add('open');
         }
     }
@@ -27,19 +24,17 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
 document.addEventListener('DOMContentLoaded', function() {
     const nav = document.querySelector('.nav'); // Updated class name
-    const main = document.querySelector('.main'); // Updated class name
 
     // Function to toggle the visibility of the navigation menu
     function toggleNav() {
         if (nav.classList.contains('open')) {
             nav.style.left = '-200px';
-            main.style.marginLeft = '0';
             nav.classList.remove('open');
         } else {
             nav.style.left = '0px';
-            main.style.marginLeft = '200px';
             nav.classList.add('open');
         }
     }
@@ -60,31 +55,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    const nav = document.querySelector('.nav3');
-    const main = document.querySelector('.main3');
+    const nav = document.querySelector('.nav3'); // Updated class name
 
+    // Function to toggle the visibility of the navigation menu
     function toggleNav() {
         if (nav.classList.contains('open')) {
             nav.style.left = '-200px';
-            main.style.marginLeft = '0';
             nav.classList.remove('open');
         } else {
             nav.style.left = '0px';
-            main.style.marginLeft = '200px';
             nav.classList.add('open');
         }
     }
 
+    // Event listener to toggle the navigation menu when clicking
     document.addEventListener('click', function(event) {
-        if (event.clientX < 200) {
+        if (event.clientX < 200) { // Clicked on the left side of the screen
             toggleNav();
-        } else if (event.target.closest('.nav3') === null) {
+        } else if (event.target.closest('.nav3') === null) { // Clicked outside the navigation
             if (nav.classList.contains('open')) {
                 toggleNav();
             }
         }
     });
 });
+
 
 
 
@@ -115,24 +110,16 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const nav = document.querySelector('.nav-about'); // Updated class name
-    const main = document.querySelector('.main-about'); // Updated class name
 
     // Function to toggle the visibility of the navigation menu
     function toggleNav() {
         if (nav.classList.contains('open')) {
             nav.style.left = '-200px';
-            main.style.marginLeft = '0';
             nav.classList.remove('open');
         } else {
             nav.style.left = '0px';
-            main.style.marginLeft = '200px';
             nav.classList.add('open');
         }
     }
@@ -147,4 +134,72 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+});
+
+
+
+
+
+
+
+// JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+    const games = {
+        action: [
+            { name: "Sea of Thieves", image: "img/SEA.png", link: "https://www.seaofthieves.com/" },
+            { name: "Valorant", image: "img/Val.png", link: "https://playvalorant.com/en-gb/" },
+            { name: "CS:GO", image: "img/Count.png", link: "https://blog.counter-strike.net/" },
+            { name: "Apex Legends", image: "img/Apex.png", link: "https://www.ea.com/en-gb/games/apex-legends" }
+        ],
+        adventure: [
+           
+            { name: "Minecraft", image: "img/mine.png", link: "https://www.minecraft.net/en-us" },
+            { name: "Terraria", image: "img/terr.png", link: "https://terraria.org/" },
+            { name: "No Man's Sky", image: "img/no.png", link: "https://www.nomanssky.com/" }
+        ],
+        strategy: [
+            { name: "League of Legends", image: "img/leauge 2.png", link: "https://www.leagueoflegends.com/en-gb/" },
+            { name: "Rocket League", image: "img/Rocket.png", link: "https://www.rocketleague.com/en" },
+            { name: "Destiny", image: "img/des.png", link: "https://www.bungie.net/7/en/destiny" },
+            { name: "World of Warcraft", image: "img/WoW.png", link: "https://worldofwarcraft.blizzard.com/en-us/" },
+            { name: "Rainbow Six Siege", image: "img/Rainbow2.png", link: "https://www.ubisoft.com/en-gb/game/rainbow-six/siege" }
+        ]
+    };
+
+    const rekomendation = document.querySelector('.rekomendation');
+    const main = document.querySelector('.main3');
+    const checkboxes = rekomendation.querySelectorAll('.genre-filter input[type="checkbox"]');
+    const searchButton = rekomendation.querySelector('.search-button');
+
+    // Function to render recommended games based on selected genres
+    function renderRecommendations() {
+        main.innerHTML = '';
+        const selectedGenres = Array.from(checkboxes)
+            .filter(checkbox => checkbox.checked)
+            .map(checkbox => checkbox.value);
+
+        let recommendedGames = [];
+        selectedGenres.forEach(genre => {
+            recommendedGames = recommendedGames.concat(games[genre]);
+        });
+
+        // Render recommended games
+        recommendedGames.forEach(game => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            const image = document.createElement('img');
+            image.src = game.image;
+            const link = document.createElement('a');
+            link.href = game.link;
+            link.appendChild(image);
+            card.appendChild(link);
+            const name = document.createElement('div');
+            name.textContent = game.name;
+            card.appendChild(name);
+            main.appendChild(card);
+        });
+    }
+
+    // Event listener for search button click
+    searchButton.addEventListener('click', renderRecommendations);
 });
